366f7a641c8fa02897fe2fe3a1a639ffe5d801b7
CALLlA-74 <56507954+CALLlA-74@users.noreply.github.com>
2026-02-26 21:15:02 +0300

    Revert "init commit"
    This reverts commit 9cf9d72df088b0cf403a97a6de2d24ba3ab82f9f.

--- internal/config/config.go
+++
@@ -1,8 +0,0 @@
-package config
-
-type Config struct {
-	HTTP struct {
-		IP   string `yaml:"ip" env:"HTTP-IP" env-default:"localhost"`
-		Port string `yaml:"port" env:"HTTP-PORT" env-default:"8080"`
-	} `yaml:"http"`
-}
--- internal/domain/usecases.go
+++
@@ -1,7 +0,0 @@
-package domain
-
-type ChangingMoneyUC struct{}
-
-func (uc *ChangingMoneyUC) ChangeMoney(cassettes []Cassette, sum int64) (map[int]int64, error) {
-	panic("")
-}
